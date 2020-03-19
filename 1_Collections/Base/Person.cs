53945b9fc68bb12edda36dc2e238fceeb944967a
Татьяна Жигадло <t.zhyhadlo@gmail.com>
2020-03-19 23:03:02 +0200

    Add project files.

---
+++ 1_Collections/Base/Person.cs
@@ -0,0 +1,15 @@
+﻿namespace ITEA_Collections.Base
+{
+    public class Person : IPerson
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public int Q { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name}: {Age}";
+        }
+    }
+}
